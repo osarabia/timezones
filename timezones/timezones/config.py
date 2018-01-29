251eb84387cdf052586ee6da5c6f4f46b48c8379
Omar Sarabia <osarabia@contractor.ea.com>
2018-01-29 07:21:40 -0700

    updating DSN

--- timezones/timezones/config.py
+++ timezones/timezones/config.py
@@ -1,2 +1,1 @@
-#DSN = "host='db_cities' dbname='postgres' user='postgres' password='password'"
-DSN = "host='172.17.0.1' dbname='postgres' user='postgres' password='password'"
+DSN = "host='db_cities' dbname='postgres' user='postgres' password='password'"
